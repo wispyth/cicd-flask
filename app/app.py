c77ba9f3bf092485abbb220c32da859531d7b210
wispyth <wispyth@gmail.com>
2025-11-13 18:36:54 +0300

    fixed flake8 linting errors

--- app/app.py
+++ app/app.py
@@ -2,19 +2,26 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "test CI/CD"
 
+
 @app.route('/health')
 def health_check():
+    """Эндпоинт для проверки здоровья сервиса (health check)."""
     return jsonify(status="OK"), 200
+
 
 @app.route('/metrics')
 def metrics():
-    return "# HELP fake_metric A fake metric for demonstration\n" \
-           "# TYPE fake_metric gauge\n" \
-           "fake_metric 100\n", 200, {'Content-Type': 'text/plain; version=0.0.4'}
+    metrics_data = (
+        "# HELP fake_metric A fake metric for demonstration\n"
+        "# TYPE fake_metric gauge\n"
+        "fake_metric 100\n"
+    )
+    return metrics_data, 200, {'Content-Type': 'text/plain; version=0.0.4'}
 
 
 if __name__ == '__main__':
